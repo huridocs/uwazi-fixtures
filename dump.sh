263044a2d16925f46c0713a62f666c2a533004ed
daneryl <daneryl@gmail.com>
2016-04-19 20:43:59 -0500

    uwazi testing fixtures and utilities to dump/restore

---
+++ dump.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+./node_modules/couchdb-dump/bin/cdbdump -d uwazi > uwazi.json
---
+++ restore.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+curl -X DELETE http://127.0.0.1:5984/uwazi/
+sleep 1
+curl -X PUT http://127.0.0.1:5984/uwazi/
+./node_modules/couchdb-dump/bin/cdbload -d uwazi < uwazi.json
