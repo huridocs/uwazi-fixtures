b3a726fdaa1349455a5bacb4c264df441a7c9f07
daneryl <daneryl@gmail.com>
2016-05-04 10:28:55 -0500

    restore.sh accepts a param as the database, default uwazi

--- restore.sh
+++ restore.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-curl -X DELETE http://localhost:9200/uwazi/
-curl -X DELETE http://127.0.0.1:5984/uwazi/
+curl -X DELETE http://localhost:9200/${1:-uwazi}/
+curl -X DELETE http://127.0.0.1:5984/${1-uwazi}/
 sleep 1
-curl -X PUT http://127.0.0.1:5984/uwazi/
-./node_modules/couchdb-dump/bin/cdbload -d uwazi < uwazi.json
+curl -X PUT http://127.0.0.1:5984/${1-uwazi}/
+./node_modules/couchdb-dump/bin/cdbload -d ${1:-uwazi} < uwazi.json
