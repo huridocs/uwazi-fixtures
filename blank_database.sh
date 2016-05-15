222fddca848472bd9c94b0ee8a4f67c74bc84106
daneryl <daneryl@gmail.com>
2016-05-15 16:31:55 -0500

    elasticSearch mappings on restore.sh and blank_database.sh

--- blank_database.sh
+++ blank_database.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-curl -X DELETE http://localhost:9200/${1:-uwazi_development}/
-sleep 1
-curl -X PUT http://localhost:9200/${1:-uwazi_development}/
+./reset_elastic_index.sh ${1:-uwazi_development}
+
+echo -e "\n\nDeleting ${1:-uwazi_development} database"
 curl -X DELETE http://127.0.0.1:5984/${1:-uwazi_development}/
 sleep 1
+echo -e "\nCreating ${1:-uwazi_development} database"
 curl -X PUT http://127.0.0.1:5984/${1-uwazi_development}/
+echo -e "\nimporting uwazi_blank_state.json into ${1:-uwazi_development} database"
 ./node_modules/couchdb-dump/bin/cdbload -d ${1:-uwazi_development} < uwazi_blank_state.json
--- restore.sh
+++ restore.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-curl -X DELETE http://localhost:9200/${1:-uwazi_development}/
+
+./reset_elastic_index.sh ${1:-uwazi_development}
+
+echo -e "\n\nDeleting ${1:-uwazi_development} database"
 curl -X DELETE http://127.0.0.1:5984/${1:-uwazi_development}/
 sleep 1
+echo -e "\nCreating ${1:-uwazi_development} database"
 curl -X PUT http://127.0.0.1:5984/${1-uwazi_development}/
+echo -e "\nimporting uwazi.json into ${1:-uwazi_development} database"
 ./node_modules/couchdb-dump/bin/cdbload -d ${1:-uwazi_development} < uwazi.json
