dc7b7dccb9e9d8c71c7d37c91da42ec8dc0774ca
daneryl <daneryl@gmail.com>
2016-05-30 15:19:49 -0500

    this fixtures now only contains test data only, not designs/views

--- dump.sh
+++ dump.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 ./node_modules/couchdb-dump/bin/cdbdump -d uwazi_development > uwazi.json
 
+#!/bin/bash
+./node_modules/couchdb-dump/bin/cdbdump -d uwazi_development | ./node_modules/couchdb-dump/bin/cdbmorph -f ./without_designs.js > uwazi.json
+
+
