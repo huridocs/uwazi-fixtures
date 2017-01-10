8b6ca266a0ba11dffa546774e1cd85139b947d0a
A Happy Cat <acasadotorres@gmail.com>
2017-01-10 18:24:19 +0100

    updated restore script to also insert views

--- restore.sh
+++ restore.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-
-./reset_elastic_index.sh ${1:-uwazi_development}
-
 echo -e "\n\nDeleting ${1:-uwazi_development} database"
 curl -X DELETE http://127.0.0.1:5984/${1:-uwazi_development}/
 sleep 1
@@ -10,6 +7,9 @@ echo -e "\nCreating ${1:-uwazi_development} database"
 curl -X PUT http://127.0.0.1:5984/${1-uwazi_development}/
 echo -e "\nimporting uwazi.json into ${1:-uwazi_development} database"
 ./node_modules/couchdb-dump/bin/cdbload -d ${1:-uwazi_development} < uwazi.json
+sleep 1
+echo -e "\nreseting views on ${1:-uwazi_development} database"
+../couchdb/restore_views.sh
 
 echo "Restoring uploaded files..."
 rm ../uploaded_documents/*.pdf
