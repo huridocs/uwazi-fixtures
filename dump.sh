fa9a96679659303247d676f447eaefcd6615ecbf
jaumec@gmail.com <jaumec@gmail.com>
2020-05-26 07:20:40 -0600

    Updated db dump and dump script

--- dump.sh
+++ dump.sh
@@ -2,8 +2,6 @@
 mongodump -h ${2:-127.0.0.1} --db ${1:-uwazi_development} -o dump
 
 echo "Copying uploaded files...";
-rm ./uploaded_documents/*.pdf
-cp ../uploaded_documents/*.pdf ./uploaded_documents
+rm ./uploaded_documents/*
+cp ../uploaded_documents/* ./uploaded_documents
 echo "DONE !";
-
-
